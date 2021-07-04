8136fcc651d65991ef9b3778eaf9e1ed6922a673
Valentine <valentine.samoylov@gmail.com>
2021-07-04 17:14:07 +0300

    Fixed nav-menu.js

--- src/js/components/_nav-menu.js
+++ src/js/components/_nav-menu.js
@@ -1,31 +1,19 @@
 // Nav menu
-"use strict"
-
-let navBtn = document.querySelector('.js-nav-btn');
-let navMenu = document.querySelector('.js-nav-menu');
-let navLinks = document.querySelectorAll('.header__link');
-let body = document.querySelector('body');
-var i;
-
-export const closeNav = () => {
-  navBtn.classList.remove('active');
-  navMenu.classList.remove('active');
-  navMenu.classList.remove('active');
-  body.classList.remove('lock');
-}
-
-export const openNav = () => {
-  navBtn.classList.add('active');
-  navMenu.classList.add('active');
-  body.classList.add('lock');
-}
+const navBtn = document.querySelector('.js-nav-btn');
+const navMenu = document.querySelector('.js-nav-menu');
+const navLinks = document.querySelectorAll('.header__link');
+const body = document.querySelector('body');
 
 export const toggleNav = () => {
   if (navBtn.classList.contains('active')) {
-    closeNav();
+    navBtn.classList.remove('active');
+    navMenu.classList.remove('active');
+    body.classList.remove('lock');
 
   } else {
-    openNav();
+    navBtn.classList.add('active');
+    navMenu.classList.add('active');
+    body.classList.add('lock');
   }
 }
 
@@ -33,9 +21,8 @@ navBtn.addEventListener('click', () => {
   toggleNav();
 });
 
-
-for (i=0; i < navLinks.length; ++i) {
+for (let i = 0; i < navLinks.length; ++i) {
   navLinks[i].addEventListener('click', () => {
-    closeNav();
+    toggleNav();
   });
 }
